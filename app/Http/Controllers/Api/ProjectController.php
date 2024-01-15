0a2b085c829bb27da6a19475c019d1e52840520c
Alati11 <peppealati@gmail.com>
2024-01-15 16:16:40 +0100

    ADD - correction call ProjectController Api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
     public function index(Request $request)
     {
 
-        $results = Project::with('technology', 'types', 'technology.types')->get();
-        // $results = Project::all();
+        // $results = Project::with('technology', 'types', 'technology.projects')->paginate(10);
+        $results = Project::all();
 
         return response()->json([
             'results' => $results,
